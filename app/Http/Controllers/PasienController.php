34794c78dc6ac9708dbf351981b0a610294e891b
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-12-11 17:42:18 +0700

    Add Pasien model

---
+++ app/Http/Controllers/PasienController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pasien;
+use Illuminate\Http\Request;
+
+class PasienController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pasien  $pasien
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pasien $pasien)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pasien  $pasien
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pasien $pasien)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pasien  $pasien
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pasien $pasien)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pasien  $pasien
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pasien $pasien)
+    {
+        //
+    }
+}
---
+++ database/migrations/2020_12_11_174156_create_pasiens_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePasiensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasiens', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasiens');
+    }
+}
