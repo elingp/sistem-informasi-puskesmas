d01c8d638b7f0963abceca531c94aae010be2551
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-12-11 18:12:34 +0700

    Add one-to-one relationship between User and Pasien

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Pasien extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function pasien()
+    {
+        return $this->hasOne(Pasien::class);
+    }
 }
